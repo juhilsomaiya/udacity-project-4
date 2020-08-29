abf89c854e9d9d355f56f84cf5e62a3c36c5615f
Juhil Somaiya <jgs.131296@gmail.com>
2020-08-29 11:55:48 +0000

    added project files

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+export dockerpath=imjuhilsomaiya/house_prediction
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login --username imjuhilsomaiya
+docker tag house_prediction $dockerpath
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
